1d934e37b9f06bb1610fbca868dc871ab4168673
Jay <jayghughri1@gmail.com>
2025-04-17 11:50:40 +0530

    test

--- modules/passive.py
+++ modules/passive.py
@@ -3,35 +3,32 @@ import json
 
 def enumerate(domain):
     """
-    
-    Perform Passive Subdomain enumerating using crt,.sh
-    
+    Perform passive subdomain enumeration using crt.sh
+
     Args:
         domain (str): The domain to enumerate
-        
-    Returns:
-        set: A set of all discovered subdomains
-        
-    """
-    
-    subdomains = set()
-    try:
-        print("[*] Querying crt.sh ... ")
-        # use crt.sh API to search for SSL certificates containing subdomains 
-        url = f"https://crt.sh/?q=%25.{domain}&output=json"
-        response=requests.get(url,timeout=10)
 
-        # if the request is successful , parse the JSON reponse
-        
+    Returns:
+        set: A set of discovered subdomains
+    """
+    subdomains = set()
+
+    try:
+        print("[*] Querying crt.sh ...")
+        # Use crt.sh API to search for SSL certificates containing subdomains
+        url = f"https://crt.sh/?q=%25.{domain}&output=json"
+        response = requests.get(url, timeout=10)
+
+        # If the request is successful, parse the JSON response
         if response.ok:
             data = json.loads(response.text)
             for entry in data:
-                name = entry.get("name_value","")
-                for sub in name.split("\\n")
-                    # add subdomains related to the domain
+                name = entry.get("name_value", "")
+                for sub in name.split("\\n"):
+                    # Add subdomains related to the domain
                     if domain in sub:
                         subdomains.add(sub.strip())
     except Exception as e:
-        print(f"[!] Error Querying crt.sh {e}")
+        print(f"[!] Error querying crt.sh: {e}")
 
     return subdomains
